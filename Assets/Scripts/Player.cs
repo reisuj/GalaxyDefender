021c2d74c880f276cd7b7c48334ed8cafdf8d261
reisuj <reisuj@gmail.com>
2024-12-30 16:07:50 -0500

    Created player using cube with movement and boundaries on X and Y axis

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() 
+    {   
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * _speed);
+        
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * _speed);
+
+        //Constrain the player's movement on the y-axis
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4, 1), 0);
+
+        //Wrap the player's movement on the x-axis
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+}
