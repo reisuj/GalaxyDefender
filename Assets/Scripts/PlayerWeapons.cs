faa21f8394c2fb75f7e9fc19ed1706aa9acc7c73
reisuj <reisuj@gmail.com>
2025-01-02 12:47:15 -0500

    Added Triple Shot laser behaviour

--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -5,8 +5,12 @@ public class PlayerWeapons : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
     private float _fireRate = 0.25f;
     private float _nextFire = 0.0f;
+    [SerializeField]
+    private bool _tripleShotActive = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,14 +23,27 @@ public class PlayerWeapons : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire)
         {
-            FireLaser();
+            _nextFire = Time.time + _fireRate;
+            if (_tripleShotActive)
+            {
+                FireTripleShot();
+            }
+            else
+            {
+                FireLaser();
+            }
         }
     }
 
     private void FireLaser()
-    {
-        _nextFire = Time.time + _fireRate;
+    {        
         GameObject playerLaser = Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.25f, 0), Quaternion.identity);
+        playerLaser.tag = "PlayerLaser";
+    }
+
+    private void FireTripleShot()
+    {        
+        GameObject playerLaser = Instantiate(_tripleShotPrefab, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
         playerLaser.tag = "PlayerLaser";
     }
 }
